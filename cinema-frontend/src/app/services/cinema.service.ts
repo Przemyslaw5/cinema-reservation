64a0df3c6e3b5551562c713eaf068c02c887da2f
Przemyslaw5 <przemyslaw.watroba@gmail.com>
2021-02-28 15:23:23 +0100

    prepare configuration for api routing

--- cinema-frontend/src/app/services/cinema.service.ts
+++ cinema-frontend/src/app/services/cinema.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
 import { Movie } from '../model/movie';
 
 @Injectable({
@@ -9,7 +10,7 @@ import { Movie } from '../model/movie';
 
 export class CinemaService {
 
-  DOMAIN = 'http://localhost:8080';
+  DOMAIN = environment.apiBase;
 
   constructor(
     private httpClient: HttpClient
