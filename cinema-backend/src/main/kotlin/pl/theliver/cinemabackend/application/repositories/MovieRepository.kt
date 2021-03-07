4bebe3fbd5c4008fb022190af07d17d86469ed66
Przemyslaw5 <przemyslaw.watroba@gmail.com>
2021-03-07 18:21:32 +0100

    Add menu to login and register in navbar #19

--- cinema-backend/src/main/kotlin/pl/theliver/cinemabackend/application/repositories/MovieRepository.kt
+++ cinema-backend/src/main/kotlin/pl/theliver/cinemabackend/application/repositories/MovieRepository.kt
@@ -1,8 +1,6 @@
 package pl.theliver.cinemabackend.application.repositories
 
 import pl.theliver.cinemabackend.domain.Movie
-import java.util.*
-
 
 interface MovieRepository {
     fun saveMovie(movie: Movie)
